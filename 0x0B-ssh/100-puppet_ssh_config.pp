e10ce1d206c8fc481d54ccd96846dd99e870f133
ridhwan <ridmartrite@gmail.com>
2023-01-13 18:19:38 -0800

    adding task 4

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#client config with puppet
+
+file_line { 'turn of passwd auth':
+  ensure  =>  present,
+  path    => '/etc/ssh/ssh_config',
+  line    =>  'PasswordAuthentication no',
+  replace => true,
+}
+
+file_line { 'Declare identity file':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => 'IdentifyFile ~/.ssh/school'
+  replace => true
+}
